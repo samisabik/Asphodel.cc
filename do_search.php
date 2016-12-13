f80e691888657c25e8b84b51fe7d5d8f385d5c2a
Sami <sabik.sami@gmail.com>
2016-12-14 04:27:08 +1100

    add

--- do_search.php
+++ do_search.php
@@ -4,20 +4,20 @@ if (isset($_POST['search'])) {
     $word = mysqli_real_escape_string($_POST['search']);
     $word = htmlentities($word);
 
+    $sql = "SELECT name FROM Part WHERE content LIKE '%" . $word . "%' ORDER BY title LIMIT 10";
+    $row = $connection->select_list($sql);
+    
 
-
-     $sql = "SELECT name FROM Part WHERE content LIKE '%" . $word . "%' ORDER BY title LIMIT 10";
-     $row = $connection->select_list($sql);
-     if(count($row)) {
-     $end_result = '';
-    foreach($row as $r) {
-             $result         = $r['storageLocation_id'];
-             // we will use this to bold the search word in result
-             $bold           = '<span class="found">' . $word . '</span>';    
-             $end_result     .= '<li>' . str_ireplace($word, $bold, $result) . '</li>';            
-         }
-         echo $end_result;
-     } else {      echo '<li>No results found</li>';
-     }
+    // if(count($row)) {
+    // $end_result = '';
+    // foreach($row as $r) {
+    //          $result         = $r['storageLocation_id'];
+    //          // we will use this to bold the search word in result
+    //          $bold           = '<span class="found">' . $word . '</span>';    
+    //          $end_result     .= '<li>' . str_ireplace($word, $bold, $result) . '</li>';            
+    //      }
+    //      echo $end_result;
+    //  } else {      echo '<li>No results found</li>';
+    //  }
  }
 ?>
