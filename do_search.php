efa875e00e24cd86fa9149ebecd7daa45afb7038
Sami <sabik.sami@gmail.com>
2016-12-14 05:20:44 +1100

    add

--- do_search.php
+++ do_search.php
@@ -15,9 +15,10 @@ if (isset($_POST['search'])) {
     //$result_2 = db_query("SELECT name FROM StorageLocation WHERE ID = %" . $row['storageLocation_id'] . "%");
     //$result_2 = db_query("SELECT name FROM StorageLocation WHERE ID = 15");
     
-    $id = mysqli_result(db_query("SELECT name FROM StorageLocation WHERE ID = 15"),0);
+    //$id = mysqli_result(db_query("SELECT name FROM StorageLocation WHERE ID = 15"),0);
 
-    echo $id;
+    $product_name = db_query("SELECT name FROM StorageLocation WHERE ID = 15")->fetch_object()->name; 
+    print $product_name; //output value
     echo "<h1>";
     echo "</h1><br>";
 } 
