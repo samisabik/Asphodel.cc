a738129e6dcebcf246652ab9a54a1e3eb64babd8
Sami <sabik.sami@gmail.com>
2016-12-14 04:22:26 +1100

    add

--- do_search.php
+++ do_search.php
@@ -1,26 +1,28 @@
 <?php
+include('db.php');
+
 //if we got something through $_POST
-if (isset($_POST['search'])) {
-    // here you would normally include some database connection
-    include('db.php');
-    // never trust what user wrote! We must ALWAYS sanitize user input
-    $word = mysql_real_escape_string($_POST['search']);
-    $word = htmlentities($word);
-    // build your search query to the database
-    $sql = "SELECT name FROM Part WHERE content LIKE '%" . $word . "%' ORDER BY title LIMIT 10";
-    // get results
-    $row = $connection->select_list($sql);
-    if(count($row)) {
-        $end_result = '';
-        foreach($row as $r) {
-            $result         = $r['storageLocation_id'];
-            // we will use this to bold the search word in result
-            $bold           = '<span class="found">' . $word . '</span>';    
-            $end_result     .= '<li>' . str_ireplace($word, $bold, $result) . '</li>';            
-        }
-        echo $end_result;
-    } else {
-        echo '<li>No results found</li>';
-    }
-}
+// if (isset($_POST['search'])) {
+//     // here you would normally include some database connection
+//     include('db.php');
+//     // never trust what user wrote! We must ALWAYS sanitize user input
+//     $word = mysql_real_escape_string($_POST['search']);
+//     $word = htmlentities($word);
+//     // build your search query to the database
+//     $sql = "SELECT name FROM Part WHERE content LIKE '%" . $word . "%' ORDER BY title LIMIT 10";
+//     // get results
+//     $row = $connection->select_list($sql);
+//     if(count($row)) {
+//         $end_result = '';
+//         foreach($row as $r) {
+//             $result         = $r['storageLocation_id'];
+//             // we will use this to bold the search word in result
+//             $bold           = '<span class="found">' . $word . '</span>';    
+//             $end_result     .= '<li>' . str_ireplace($word, $bold, $result) . '</li>';            
+//         }
+//         echo $end_result;
+//     } else {
+//         echo '<li>No results found</li>';
+//     }
+// }
 ?>
