8a80565a5009ba4b89c5b164a1292d0122c71071
Sami <sabik.sami@gmail.com>
2016-12-14 04:24:24 +1100

    add

--- do_search.php
+++ do_search.php
@@ -1,20 +1,11 @@
 <?php
 if (isset($_POST['search'])) {
     include('db.php');
-
+    $word = mysql_real_escape_string($_POST['search']);
+    $word = htmlentities($word);
 }
 
 
-
-
-
-//if (isset($_POST['search'])) {
-//     // here you would normally include some database connection
-//     include('db.php');
-//     // never trust what user wrote! We must ALWAYS sanitize user input
-//     $word = mysql_real_escape_string($_POST['search']);
-//     $word = htmlentities($word);
-//     // build your search query to the database
 //     $sql = "SELECT name FROM Part WHERE content LIKE '%" . $word . "%' ORDER BY title LIMIT 10";
 //     // get results
 //     $row = $connection->select_list($sql);
