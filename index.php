6e05f80bd37d8e34a13fc123dbc8d37d4c543012
Sami <sabik.sami@gmail.com>
2016-12-14 00:27:03 +1100

    commit local files

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Asphodel Meadows" />
+    <title>Asphodel.cc</title>
+    <link rel="stylesheet" href="style/style.css" type="text/css" />
+    <link href='http://fonts.googleapis.com/css?family=Inconsolata:400,700' rel='stylesheet' type='text/css' />
+  
+</head>
+
+<body>
+<?php
+
+function db_connect() {
+
+    // Define connection as a static variable, to avoid connecting more than once 
+    static $connection;
+
+    // Try and connect to the database, if a connection has not been established yet
+    if(!isset($connection)) {
+         // Load configuration as an array. Use the actual location of your configuration file
+        $config = parse_ini_file('config.ini'); 
+        $connection = mysqli_connect('localhost',$config['username'],$config['password'],$config['dbname']);
+    }
+
+    // If connection was not successful, handle the error
+    if($connection === false) {
+        // Handle error - notify administrator, log to a file, show an error screen, etc.
+        return mysqli_connect_error(); 
+    }
+    return $connection;
+}
+
+function db_query($query) {
+    // Connect to the database
+    $connection = db_connect();
+
+    // Query the database
+    $result = mysqli_query($connection,$query);
+
+    return $result;
+}
+
+function db_error() {
+    $connection = db_connect();
+    return mysqli_error($connection);
+}
+
+$connection = db_connect();
+
+$result = db_query("SELECT name FROM Part");
+if($result === false) {
+	$error = db_error();
+} else {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<h1>"
+        echo $row['name'];
+        echo "</h1><br>";
+    }
+}
+?>
+</body>
+</html>
