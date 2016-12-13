0ce73f64c05b27167491b2b621fbca67ace0d5ac
Sami <sabik.sami@gmail.com>
2016-12-14 04:34:01 +1100

    add

--- do_search.php
+++ do_search.php
@@ -10,19 +10,19 @@ if($result === false) {
     $error = db_error();
 } else {
 
+    $row = mysql_fetch_array($result)
     
-
-    // if(count($row)) {
-    // $end_result = '';
-    // foreach($row as $r) {
-    //          $result         = $r['storageLocation_id'];
-    //          // we will use this to bold the search word in result
-    //          $bold           = '<span class="found">' . $word . '</span>';    
-    //          $end_result     .= '<li>' . str_ireplace($word, $bold, $result) . '</li>';            
-    //      }
-    //      echo $end_result;
-    //  } else {      echo '<li>No results found</li>';
-    //  }
+    if(count($row)) {
+    $end_result = '';
+    foreach($row as $r) {
+             $result         = $r['storageLocation_id'];
+             // we will use this to bold the search word in result
+             $bold           = '<span class="found">' . $word . '</span>';    
+             $end_result     .= '<li>' . str_ireplace($word, $bold, $result) . '</li>';            
+         }
+         echo $end_result;
+     } else {      echo '<li>No results found</li>';
+     }
 }
  }
 ?>
